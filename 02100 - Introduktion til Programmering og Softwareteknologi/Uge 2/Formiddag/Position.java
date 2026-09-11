85738d7a18a5953d3410a7eb43604ea6b5473005
Melo <dulabamba@proton.me>
2026-09-11 11:42:47 +0200

    U2, F, O1

---
+++ 02100 - Introduktion til Programmering og Softwareteknologi/Uge 2/Formiddag/Position.java
@@ -0,0 +1,14 @@
+// Opgave 1
+
+public class Position {
+    public static void main(String[] args) {
+        double s0 = 1.0;
+        double v0 = 4.1;
+        double a = 3.5;
+        double t = 20.0;
+
+        double s = s0 + v0 * t + 0.5 * a * t * t;
+
+        System.out.println(s);
+    }
+}
