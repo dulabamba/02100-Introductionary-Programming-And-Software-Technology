253ef946dd1abf6f2146b116ded6cb5857e3aaa0
Melo <dulabamba@proton.me>
2026-09-11 12:12:43 +0200

    Update files

--- 02100 - Introduktion til Programmering og Softwareteknologi/Uge 2/Formiddag/Position.java
+++ 02100 - Introduktion til Programmering og Softwareteknologi/Uge 2/Formiddag/Position.java
@@ -1,8 +1,8 @@
 // Opgave 1
-
+// Test
 public class Position {
     public static void main(String[] args) {
-        double s0 = 1.0;
+        double s0 = 1.4;
         double v0 = 4.1;
         double a = 3.5;
         double t = 20.0;
