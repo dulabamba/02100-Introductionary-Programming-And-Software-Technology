0871db1c8a8ad47fea266ae4bcf425898a903e52
dulabamba <dulabamba@proton.me>
2026-09-11 14:47:52 +0200

    Rettelse af U2, F, O2

--- 02100 - Introduktion til Programmering og Softwareteknologi/Uge 2/Formiddag/PotensLoop.java
+++ 02100 - Introduktion til Programmering og Softwareteknologi/Uge 2/Formiddag/PotensLoop.java
@@ -3,11 +3,8 @@
 public class PotensLoop {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.print(i * i);
-
-            if (i < 10) {
-                System.out.print(" "); // Tilføjer et mellemrum efter hver .print pånær 10 * 10
-            }
-        }
+            System.out.print(i * i + " ");
+    }
+    System.out.println();
     }
 }
